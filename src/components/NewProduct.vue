589fcc5f3ffbecd8d7818014f03be9e7bbab9633
RESUL <resul067142@gmail.com>
2022-11-24 15:04:27 +0300

    FOTOGRAF DÜZENLEMELER EKLENDİ

--- src/components/NewProduct.vue
+++ src/components/NewProduct.vue
@@ -5,23 +5,23 @@
                 <img height="128" class="img-responsive text-center mb-3"
                      :src="product.selectedImage == null ? '/src/assets/default.png' : product.selectedImage">
                 <input ref="file" type="file" style="display: none;" @change="onChange($event)" class="form-control">
-                <button class="btn btn-outline-secondary " type="button" @click="$refs.file.click()">Resim Seç</button>
+                <button class="btn btn-outline-secondary " type="button" @click="$refs.file.click()">FOTOGRAF SEÇ</button>
             </div>
         </div>
         <div class="col-md-5">
             <div class="col-md-11 card">
                 <div class="card-body">
                     <div class="form-group">
-                        <label>Ürün Adı</label>
+                        <label>FOTOGRAF Adı</label>
                         <input type="text" v-model="product.title" class="form-control" placeholder="adını giriniz">
                     </div>
                     <div class="row">
                         <div class="form-group col-md-6">
-                            <label>Ürün Adeti</label>
+                            <label>FOTOGRAF Adeti</label>
                             <input type="text" v-model="product.count" class="form-control"
                                    placeholder="adetini giriniz">
                         </div>
-                        <div class="form-group col-md-6">
+                        <div class="form-group col-md-6"> 
                             <label>Ürün Fiyatı</label>
                             <input type="text" v-model="product.price" class="form-control"
                                    placeholder="fiyatını giriniz">
